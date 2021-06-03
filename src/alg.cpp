f392091d027b8f75a6e80263bb7ce7b6380a2860
Polina Plesovskih <78843788+tsv-mindal@users.noreply.github.com>
2021-06-03 11:39:07 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,4 +7,25 @@
 
 BST<std::string> makeTree(const char* filename) {
   // поместите сюда свой код
+BST<std::string>tree;
+    std::string word = "";
+    char lowercase = 'a' - 'A';
+    std::ifstream file(filename);
+    if (!file) {
+        std::cout << "Error!" << std::endl;
+        return tree;
+    }
+    while (!file.eof()) {
+        char ch = file.get();
+        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+            if (ch >= 'A' && ch <= 'Z')
+                ch += lowercase;
+            word += ch;
+        } else if (word != "") {
+            tree.add(word);
+            word = "";
+        }
+    }
+    file.close();
+    return tree;
 }
